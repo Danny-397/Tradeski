91e71666d1ebcaf69216dc8d269c704fae21dbc1
Dan Lichtenberger <dlichtenberger91@icloud.com>
2026-05-16 20:22:18 -0400

    Update test_notifier.py
    No more Flake 8 errors

--- tracker/test_notifier.py
+++ tracker/test_notifier.py
@@ -3,6 +3,18 @@ from tracker.config import PushoverConfig
 
 
 def test_notifier_no_credentials():
-    n = Notifier(PushoverConfig(user_key="", api_token=""))
-    # Should not raise
-    n.alert("AAPL", "test", "Test Title", "Test Message", persist=False)
+    config = PushoverConfig(
+        user_key="",
+        api_token=""
+    )
+
+    notifier = Notifier(config)
+
+    # Should not raise an exception
+    notifier.alert(
+        "AAPL",
+        "test",
+        "Test Title",
+        "Test Message",
+        persist=False,
+    )
